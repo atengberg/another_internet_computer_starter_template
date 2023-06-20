c93456c47830acd95e188b4652ce67c564bb19fc
Ashton Engberg <atengberg@proton.me>
2023-06-19 20:08:22 -0400

    added router, implemented simplest BrowserRouter with createBrowserRouter

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import CanisterProvider from './components/CanisterProvider.jsx';
 import App from './App.jsx';
 import './index.css';
 
-// Check web workers are supported.
-const supported = !!typeof(Worker);
-const getNotSupported = () => (
-  <div className="w-full mt-1/3 text-center italic text-3xl">This dapp requires a browser that supports web workers.</div>
-)
+const dapp = (
+  <CanisterProvider>
+    <App />
+  </CanisterProvider>
+);
 
-const dapp = <CanisterProvider><App /></CanisterProvider>;
-ReactDOM.createRoot(document.getElementById('root')).render(supported ? dapp : getNotSupported());
+const notSupported = (
+  <div className="w-full mt-1/3 text-center italic text-3xl">
+    This dapp requires a browser that supports web workers.
+  </div>
+);
+
+// Check web workers are supported (though CanIUse suggests this is likely unnecessary).
+const supported = !!typeof(Worker);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    index: true,
+    element: supported ? dapp : notSupported
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
