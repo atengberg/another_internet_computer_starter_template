7f1fa47c3e0429b2c6027849194542e87753ee1d
Ashton Engberg <atengberg@proton.me>
2023-06-22 16:32:01 -0400

    Moved BrowserRouter into main.jsx

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CanisterProvider from './components/CanisterProvider.jsx';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
 const dapp = (
-  <CanisterProvider>
-    <App />
-  </CanisterProvider>
+  <Router>
+    <CanisterProvider>
+      <App />
+    </CanisterProvider>
+  </Router>
 );
 
 const notSupported = (
